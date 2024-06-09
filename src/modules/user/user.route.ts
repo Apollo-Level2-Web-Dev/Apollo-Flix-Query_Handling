6e97d0e7f421455910b4dc68772a429b5cf50484
saminravi11 <saminravi.github@gmail.com>
2024-06-10 00:32:06 +0600

    auth

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,7 +1,8 @@
 /**
  * /user/create-admin [POST] for admin only
- * /user [PUT] for admin only
- * /user [DELETE] for admin only
+ * /user/:id [PUT] for admin only
+ * /user/:id [DELETE] for admin only
  * /user [GET] for admin only
+ * /user/:id [GET] for admin only
  *
  */
